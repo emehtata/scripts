1b7b03a7e3f9a69a20debc4fbd46ff3b2e980f4e
emehtata <emehtata@users.noreply.github.com>
2020-02-26 19:44:45 +0200

    Better error handling with exits

--- cygwin/clean_desktop.sh
+++ cygwin/clean_desktop.sh
@@ -3,16 +3,16 @@
 # files (no links) from your Windows Desktop
 
 # Change DEFAULTDIR or use parameter from command line
-DEFAULTDIR="/cygdrive/c/$HOMEPATH/Desktop"
+DEFAULTDIR="$HOME/Desktop"
 DIR=${1:-$DEFAULTDIR}
-cd $DIR
+cd $DIR || exit
 
 SOURCE=$(pwd)
 
 SOURCE=${SOURCE#/cygdrive/}
 
 # Change TARGET for your needs
-TARGET="e:\Archive\\"$SOURCE
+TARGET="c:\Archive\\"$SOURCE
 
 echo "SOURCE is $SOURCE."
 echo "TARGET is $TARGET."
@@ -21,8 +21,8 @@ ls
 
 GLOBIGNORE=*.lnk:*.ini:*.url
 
-mkdir -p "$TARGET"
+mkdir -p "$TARGET" || exit
 
-mv -v *.* "$TARGET"
+mv -v *.* "$TARGET" || exit
 
 unset GLOBIGNORE
