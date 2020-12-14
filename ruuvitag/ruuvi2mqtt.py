7313b2d85015c2cf6f488814b14d4385a3ab1c9b
Esa M <emehtata@users.noreply.github.com>
2020-12-14 16:54:28 +0200

    use redis to track missing ruuvi tags

--- ruuvitag/ruuvi2mqtt.py
+++ ruuvitag/ruuvi2mqtt.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import redis
+import struct
+import time
 import paho.mqtt.client as mqtt
 import json
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARNING)
 
 client=mqtt.Client("Ruuvireader")
 client.connect("localhost")
@@ -20,15 +23,43 @@ ruuvis = {
   "FE:87:0F:93:69:AA": "biergarten"
   }
 
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+for tag in ruuvis:
+  r.set(ruuvis[tag]+'.seen', time.time())
+
+def set_redis_last(key, lastseen):
+  try:
+    r.set(key+'.seen', lastseen)
+    logging.debug("key: "+key+", seen: "+str(lastseen))
+  except:
+    logging.error("Can not set redis value")
+
+def get_redis_last(key):
+  retval=float(r.get(key+'.seen'))
+  logging.debug("%s %f" % (key, retval))
+  if retval:
+    return retval
+  else:
+    return time.time()
+
 def handle_data(found_data):
   room=ruuvis[found_data[0]]
   topic="home/"+room
-  print(room)
+  logging.debug(room)
   jdata=found_data[1]
   jdata.update( { "room": room } )
   my_data=json.dumps(jdata).replace("'", '"')
-  print(my_data)
+  logging.debug(my_data)
   client.publish(topic, my_data)
+  set_redis_last(room, time.time())
+  for tag in ruuvis:
+    lastseen = time.time()-get_redis_last(ruuvis[tag])
+    if lastseen > 60:
+      logging.warning("Not receiving data from %s" % tag)
+      logging.error("Restarting")
+      os.execv(sys.argv[0], sys.argv)
+    else:
+      logging.debug("%s last seen %f seconds ago" % ( tag, lastseen ) )
 
 RuuviTagSensor.get_datas(handle_data)
-
