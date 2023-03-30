5e78e4c69232a7d89ac3388a338ab895f2a5f11a
Esa Mehtätalo <emehtata@users.noreply.github.com>
2023-03-30 15:49:29 +0300

    Separate settings

--- ruuvitag/ruuvi2mqtt.py
+++ ruuvitag/ruuvi2mqtt.py
@@ -10,29 +10,15 @@ import sys
 import os
 import platform
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
+from settings import brokers
+from settings import ruuvis
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.WARNING,
+    level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
 clients = {}
-
-brokers = {
-    "192.168.7.8":  { "port": 11883 }
-}
-
-ruuvis = {
-  "DD:17:F3:D7:86:CE": "pool",
-  "EA:D5:76:69:70:99": "freezer",
-  "EC:67:46:36:EA:60": "sauna",
-  "FE:87:0F:93:69:AA": "biergarten",
-  "E8:28:93:CE:5A:E8": "greenhouse",
-  "D5:43:48:93:FE:F0": "fridge",
-  "C5:7D:4C:65:9D:60": "car-interior",
-  "E2:9D:53:95:0E:8B": "upstairs-small-bedroom",
-  "D1:48:D2:7D:3D:02": "downstairs-bedroom"
-  }
 
 def send_single(jdata, keyname, client):
   topic=jdata['room']+f"/{keyname}"
