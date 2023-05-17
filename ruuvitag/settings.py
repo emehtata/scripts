31aed08015e515b46e363fd92d16350393392c98
Esa Mehtätalo <emehtata@users.noreply.github.com>
2023-05-17 12:48:28 +0300

    Fix settings

--- ruuvitag/settings.py
+++ ruuvitag/settings.py
@@ -3,7 +3,13 @@ brokers = {
 }
 
 ruuvis = {
-  "DD:17:F3:D7:86:CE": "alamokki",
-  "E8:0D:4B:5D:BD:D8": "ylamokki",
+  "DD:17:F3:D7:86:CE": "pool",
+  "EA:D5:76:69:70:99": "freezer",
+  "EC:67:46:36:EA:60": "sauna",
+  "FE:87:0F:93:69:AA": "biergarten",
+  "E8:28:93:CE:5A:E8": "greenhouse",
+  "D5:43:48:93:FE:F0": "fridge",
+  "C5:7D:4C:65:9D:60": "car-interior",
+  "E2:9D:53:95:0E:8B": "upstairs-small-bedroom",
+  "D1:48:D2:7D:3D:02": "downstairs-bedroom"
 }
-
